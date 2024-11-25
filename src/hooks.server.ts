b26fd4df896900707e52d4c4494b9e1744679d85
Kai Volland <kaivolland@yahoo.de>
2024-11-25 15:34:00 +0100

    feat: add logging lib

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,12 @@
+import { env } from '$env/dynamic/private';
 import type { Handle } from '@sveltejs/kit';
 import handleAuth from '$lib/auth/handle';
 import { i18n } from '$lib/i18n';
 import { sequence } from '@sveltejs/kit/hooks';
+import { setLogLevel, type LogLevel } from 'loggisch';
 
 const handleParaglide: Handle = i18n.handle();
 
 export const handle: Handle = sequence(handleAuth, handleParaglide);
+
+setLogLevel(env.CLIENT_LOG_LEVEL as LogLevel || 'info');
