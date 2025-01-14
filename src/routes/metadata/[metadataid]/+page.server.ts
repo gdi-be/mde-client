04883e0303250cacccd873fc0404358877fea8d6
Kai Volland <kaivolland@yahoo.de>
2025-01-14 11:54:37 +0100

    fix: redirect to login if acces token invalid

--- src/routes/metadata/[metadataid]/+page.server.ts
+++ src/routes/metadata/[metadataid]/+page.server.ts
@@ -2,11 +2,12 @@ import { error } from '@sveltejs/kit';
 import { getMetadataCollectionByMetadataId } from '$lib/api/metadata.js';
 import { getAccessToken } from '$lib/auth/cookies.js';
 import { getFormHelp } from '$lib/api/help.js';
+import { redirect } from '@sveltejs/kit';
 
 export async function load({ params, cookies }) {
 
   const token = await getAccessToken(cookies);
-  if (!token) return error(401, 'Unauthorized');
+  if (!token) return redirect(302, '/login');
 
   const help = await getFormHelp();
   if (!help) return error(500, 'Failed to fetch form config');
