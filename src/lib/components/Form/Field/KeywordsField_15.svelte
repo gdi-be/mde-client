650654fab44fc5f92ff99bf3f1b23020ca0b9594
Kai Volland <kaivolland@yahoo.de>
2025-01-30 13:48:04 +0100

    feat: add autokeywords

--- src/lib/components/Form/Field/KeywordsField_15.svelte
+++ src/lib/components/Form/Field/KeywordsField_15.svelte
@@ -7,16 +7,35 @@
   import Paper from "@smui/paper";
   import FieldTools from "../FieldTools.svelte";
   import { invalidateAll } from "$app/navigation";
+  import { onMount } from "svelte";
 
   const KEY = 'isoMetadata.keywords';
   const LABEL = 'Schlagwörter';
 
+  let containerElement = $state<HTMLDivElement>();
   let initialKeyWords = getValue<KeyWords>(KEY);
+  let { metadataid } = page.params;
   let initialValue = initialKeyWords?.default?.map(
     (entry) => ({ key: entry.keyword, label: entry.keyword })
   ) || [];
-  let value = $state(initialValue);
+  let value = $state<Option[]>(initialValue);
   let showCheckmark = $state(false);
+  let autoKeywords = $state<string[]>([]);
+  let autoValue = $derived<Option[]>(autoKeywords.map(
+    (entry) => ({ key: entry, label: entry })
+  ));
+  let uniqueValue = $derived(Array.from(
+    new Map([...autoValue,...value].map((entry) => [entry.key, entry])).values()
+  ));
+
+  const getAutoKeywords = async () => {
+    const response = await fetch(`/metadata/${metadataid}/autokeywords`)
+    if (response.ok) {
+      autoKeywords = await response.json() || [];
+    } else {
+      autoKeywords = [];
+    }
+  };
 
   const searchItems = async (input: string) => {
     if (input === '') {
@@ -59,12 +78,36 @@
     }
   };
 
+  onMount(() => {
+    getAutoKeywords();
+  });
+
+  // this is a hack to remove the delete button from undeletable chips as the framework
+  // does not provide a way to do this
+  $effect(() => {
+    if (autoKeywords && containerElement) {
+      containerElement.querySelectorAll(".mdc-chip").forEach(chip => {
+        const textElement = chip.querySelector(".mdc-chip__text");
+        if (textElement?.textContent && autoKeywords.includes(textElement?.textContent?.trim())) {
+          const removeButton = chip.querySelector(".mdc-deprecated-chip-trailing-action");
+          if (removeButton) {
+            removeButton.remove();
+          }
+          chip.setAttribute('title', 'Automatisch generiertes Schlagwort');
+        }
+      });
+    }
+  });
+
 </script>
 
-<div class="keywords-field">
+<div
+  class="keywords-field"
+  bind:this={containerElement}
+>
   <Paper>
     <ChipInput
-      bind:chips={value}
+      chips={uniqueValue}
       fieldLabel={LABEL + '*'}
       search={searchItems}
       onChange={onChange}
@@ -81,6 +124,21 @@
     position: relative;
     display: flex;
     gap: 0.25em;
+
+    :global(.mdc-chip:not(:has(.mdc-deprecated-chip-trailing-action))) {
+      pointer-events: auto;
+      cursor: default;
+      border: 1px dashed var(--primary-color);
+      opacity: 0.7;
+    }
+
+    // Disable clicke events but keep the title attribute
+    :global(.mdc-chip:not(:has(.mdc-deprecated-chip-trailing-action)):active),
+    :global(.mdc-chip:not(:has(.mdc-deprecated-chip-trailing-action)):focus),
+    :global(.mdc-chip:not(:has(.mdc-deprecated-chip-trailing-action)) button),
+    :global(.mdc-chip:not(:has(.mdc-deprecated-chip-trailing-action)) a) {
+      pointer-events: none;
+    }
 
     :global(.smui-paper) {
       flex: 1;
