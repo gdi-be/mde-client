b8094eba25cb9c7b4d8e2bb914e2dd38bfba9419
Kai Volland <kaivolland@yahoo.de>
2025-02-03 15:36:51 +0100

    fix: filter unsupported inspire theme

---
+++ src/lib/models/inspire.ts
@@ -0,0 +1,79 @@
+export type InspireRegister = {
+  register: {
+    registry: {
+      label: {
+        text: string;
+        lang: string;
+      };
+      id: string;
+    };
+    containeditems: InspireThemeItem[];
+  };
+};
+
+export type InspireThemeItem = {
+  theme: {
+    "governance level": {
+      label: {
+        text: string;
+        lang: string;
+      };
+      id: string;
+    };
+    created: string;
+    "governance-level": {
+      label: {
+        text: string;
+        lang: string;
+      };
+      uri: string;
+    };
+    description: {
+      text: string;
+      lang: string;
+    };
+    language: string;
+    label: {
+      text: string;
+      lang: string;
+    };
+    itemclass: {
+      label: {
+        text: string;
+        lang: string;
+      };
+      id: string;
+    };
+    themenumber: string;
+    definition: {
+      text: string;
+      lang: string;
+    };
+    id: string;
+    latestversion: string;
+    thisversion: string;
+    historyversion: { version: string }[];
+    annex: string;
+    status: {
+      label: {
+        text: string;
+        lang: string;
+      };
+      id: string;
+    };
+    register: {
+      registry: {
+        label: {
+          text: string;
+          lang: string;
+        };
+        id: string;
+      };
+      label: {
+        text: string;
+        lang: string;
+      };
+      id: string;
+    };
+  };
+};
