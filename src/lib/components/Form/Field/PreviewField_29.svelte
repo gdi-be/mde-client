2ec482d30a3e50c3dddf220310f53439b59cc8ca
Kai Volland <kaivolland@yahoo.de>
2025-02-04 14:12:10 +0100

    fix: simplify preview field

--- src/lib/components/Form/Field/PreviewField_29.svelte
+++ src/lib/components/Form/Field/PreviewField_29.svelte
@@ -1,35 +1,20 @@
 <script lang="ts">
   import { page } from "$app/state";
-  import type { PreviewMap } from "$lib/models/metadata";
+  import TextInput from "$lib/components/Form/Inputs/TextInput.svelte";
+  import Paper from "@smui/paper";
   import { getValue } from "../FormContext.svelte";
   import FieldTools from "../FieldTools.svelte";
   import { invalidateAll } from "$app/navigation";
-  import NumberInput from "../Inputs/NumberInput.svelte";
-  import TextInput from "../Inputs/TextInput.svelte";
 
-  const KEY = 'isoMetadata.previewMap';
+  const KEY = 'isoMetadata.preview';
   const LABEL = 'Vorschau';
-  const LABEL_EXTENT = 'Räumliche Ausdehnung';
-  const LABEL_URL = 'URL';
-  const LABEL_CRS = 'Koordinatenreferenzsystem';
-  const LABEL_MAX_X = 'Maximaler X-Wert';
-  const LABEL_MIN_X = 'Minimaler X-Wert';
-  const LABEL_MAX_Y = 'Maximaler Y-Wert';
-  const LABEL_MIN_Y = 'Minimaler Y-Wert';
 
-  let initialValue = getValue<PreviewMap>(KEY);
-  let value = $state(initialValue  || {
-    crs: '',
-    url: '',
-    minx: NaN,
-    maxx: NaN,
-    miny: NaN,
-    maxy: NaN
-  });
+  let initialValue = getValue<string>(KEY);
+  let value = $state(initialValue || '');
   let showCheckmark = $state(false);
 
   const onBlur = async () => {
-    // TODO: check if value has changed
+    // TODO check if value has changed
     const response = await fetch(page.url, {
       method: 'PATCH',
       headers: {
@@ -41,7 +26,6 @@
       })
     });
     if (response.ok) {
-      initialValue = value;
       showCheckmark = true;
       invalidateAll();
     }
@@ -50,53 +34,15 @@
 </script>
 
 <div class="preview-field">
-  <fieldset class="wrapper-fieldset">
-    <legend>{LABEL}</legend>
-    <div class="text-inputs">
-      <TextInput
-        bind:value={value.url}
-        label={LABEL_URL}
-        onblur={onBlur}
-        required
-      />
-      <TextInput
-        bind:value={value.crs}
-        label={LABEL_CRS}
-        onblur={onBlur}
-      />
-    </div>
-    <fieldset class="extent-fieldset">
-      <legend>{LABEL_EXTENT}</legend>
-      <div class="inline-fields">
-        <NumberInput
-          bind:value={value.minx}
-          label={LABEL_MIN_X}
-          type="float"
-          onblur={onBlur}
-        />
-        <NumberInput
-          bind:value={value.maxx}
-          label={LABEL_MAX_X}
-          type="float"
-          onblur={onBlur}
-        />
-      </div>
-      <div class="inline-fields">
-        <NumberInput
-          bind:value={value.miny}
-          label={LABEL_MIN_Y}
-          type="float"
-          onblur={onBlur}
-        />
-        <NumberInput
-          bind:value={value.maxy}
-          label={LABEL_MAX_Y}
-          type="float"
-          onblur={onBlur}
-        />
-      </div>
-    </fieldset>
-  </fieldset>
+  <Paper class="input-wrapper">
+    <TextInput
+      bind:value
+      key={KEY}
+      label={LABEL}
+      onblur={onBlur}
+      required
+    />
+  </Paper>
   <FieldTools
     key={KEY}
     bind:running={showCheckmark}
@@ -105,47 +51,16 @@
 
 <style lang="scss">
   .preview-field {
+    position: relative;
     display: flex;
     gap: 0.25em;
 
-    .text-inputs {
+    :global(.input-wrapper) {
       flex: 1;
-      display: flex;
-      flex-direction: column;
-      gap: 1em;
-
-      :global(.mdc-text-field) {
-        width: 100%;
-      }
     }
 
-    .inline-fields {
+    :global(.mdc-text-field) {
       display: flex;
-      gap: 1em;
-      justify-content: space-evenly;
     }
-
-    .wrapper-fieldset {
-      display: flex;
-      justify-content: space-between;
-      flex: 1;
-      gap: 2em;
-      border-radius: 4px;
-
-      .extent-fieldset {
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-        gap: 1em;
-        border-radius: 4px;
-        justify-content: space-between;
-      }
-
-      >legend {
-        display: flex;
-        align-items: center;
-      }
-    }
-
   }
 </style>
