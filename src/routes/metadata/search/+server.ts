2b5c7f73c6decf3115eaedaa83768f0f53aeca55
Kai Volland <kaivolland@yahoo.de>
2025-03-25 14:06:49 +0100

    fix: restore old search interface

---
+++ src/routes/metadata/search/+server.ts
@@ -0,0 +1,20 @@
+import { error, json } from '@sveltejs/kit';
+import { getAccessToken } from '$lib/auth/cookies.js';
+import { searchForMetadata } from '$lib/api/metadata.js';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ cookies, url }) {
+  const token = await getAccessToken(cookies);
+  if (!token) return error(401, 'Unauthorized');
+
+  const params = url?.searchParams;
+  const query = params.get('query');
+
+  if (!query) {
+    return error(400, 'Bad Request');
+  }
+
+  const createResponse = await searchForMetadata(token, query);
+
+  return json(createResponse);
+}
