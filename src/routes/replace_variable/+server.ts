60abaa815dba145234186a645792b07204ebbe76
Kai Volland <kaivolland@yahoo.de>
2025-04-09 16:12:13 +0200

    fix: fix build

--- src/routes/replace_variable/+server.ts
+++ src/routes/replace_variable/+server.ts
@@ -1,13 +1,14 @@
 import { error } from '@sveltejs/kit';
 import { getAccessToken } from '$lib/auth/cookies.js';
 
-const file = Bun.file('/data/variables.json');
-const variables = await file.json();
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ cookies, request }) {
   const token = await getAccessToken(cookies);
   if (!token) return error(401, 'Unauthorized');
+
+  const file = Bun.file('/data/variables.json');
+  const variables = await file.json();
 
   let { value } = await request.json();
 
