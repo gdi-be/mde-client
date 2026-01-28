f8e5651a8a93a6b8d5ba307b3d59c2c94ab9d1c4
Kai Volland <kaivolland@yahoo.de>
2026-01-28 11:18:35 +0100

    fix: update path to terms_of_use_on_privacy.yaml

--- src/routes/data/terms_of_use_privacy/+server.ts
+++ src/routes/data/terms_of_use_privacy/+server.ts
@@ -6,7 +6,7 @@ import { parse } from 'yaml';
 export async function GET({ cookies }) {
   const token = await getAccessToken(cookies);
   if (!token) return error(401, 'Unauthorized');
-  const terms_of_use = Bun.file('/data/codelists/terms_of_use_privacy.yaml');
+  const terms_of_use = Bun.file('/data/codelists/terms_of_use_on_privacy.yaml');
 
   const terms = await terms_of_use.text();
   const parsed = parse(terms);
