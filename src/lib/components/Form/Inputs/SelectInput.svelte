a4b2870722aff6ecc42d77944d202b6f79a1b893
Kai Volland <kaivolland@yahoo.de>
2026-02-25 15:29:46 +0100

    chore: fix/ignore typecheck issues

--- src/lib/components/Form/Inputs/SelectInput.svelte
+++ src/lib/components/Form/Inputs/SelectInput.svelte
@@ -36,13 +36,17 @@
   }: InputProps = $props();
 
   // Remove duplicates
-  options = Array.from(new Map(options.map((item) => [item.key, item])).values());
+  const uniqueOptions = $derived(
+    Array.from(new Map(options.map((item) => [item.key, item])).values())
+  );
 
   const onSelect = (newValue: string) => {
     onChange?.(newValue);
   };
 
-  const selectedDescription = $derived(options.find((item) => item.key === value)?.description);
+  const selectedDescription = $derived(
+    uniqueOptions.find((item) => item.key === value)?.description
+  );
 
   let isInvalid = $derived.by(() => {
     if (!fieldConfig) return false;
