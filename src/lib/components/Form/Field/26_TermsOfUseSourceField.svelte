c809162d436fd8ed9464f0725d752edbbe038a55
Michael Holthausen <mholthausen@terrestris.de>
2026-05-26 12:46:18 +0200

    Revert "fix: enhance field validation for terms of use source based on privacy"
    This reverts commit ede512ced602448e18e133a0d11aa4aa70479b8d.

--- src/lib/components/Form/Field/26_TermsOfUseSourceField.svelte
+++ src/lib/components/Form/Field/26_TermsOfUseSourceField.svelte
@@ -4,13 +4,14 @@
   import { MetadataService } from '$lib/services/MetadataService';
   import FieldTools from '../FieldTools.svelte';
   import { page } from '$app/state';
+  import { ValidationService } from '$lib/services/ValidationService';
   const t = $derived(page.data.t);
 
   const KEY = 'isoMetadata.termsOfUseSource';
   const TERMS_OF_USE_KEY = 'isoMetadata.termsOfUseId';
   const PRIVACY_KEY = 'isoMetadata.privacy';
 
-  const { getValue, formState } = getFormContext();
+  const { getValue } = getFormContext();
   const valueFromData = $derived(getValue<string>(KEY));
   const termsOfUseId = $derived(getValue<number>(TERMS_OF_USE_KEY));
   const privacy = $derived(getValue<string>(PRIVACY_KEY));
@@ -20,19 +21,9 @@
     value = valueFromData || '';
   });
 
-  $effect(() => {
-    if (!formState.metadata?.isoMetadata) return;
-    formState.metadata.isoMetadata.termsOfUseSource = value;
-  });
-
   let showCheckmark = $state(false);
   const fieldConfig = MetadataService.getFieldConfig<string>(26);
-  let validationResult = $derived(
-    fieldConfig?.validator(value, {
-      'isoMetadata.termsOfUseId': termsOfUseId,
-      'isoMetadata.privacy': privacy
-    })
-  );
+  let validationResult = $derived(ValidationService.validateField(fieldConfig, value));
 
   const onBlur = async () => {
     if (validationResult?.valid === false) return;
--- src/lib/components/Form/FieldsConfig.ts
+++ src/lib/components/Form/FieldsConfig.ts
@@ -526,11 +526,10 @@ export const FieldConfigs: FullFieldConfig<any>[] = [
   {
     profileId: 26,
     key: 'isoMetadata.termsOfUseSource',
-    extraParams: ['isoMetadata.termsOfUseId', 'isoMetadata.privacy'],
+    extraParams: ['isoMetadata.termsOfUseId'],
     validator: (val, extraParams) => {
       const termsOfUseId = extraParams?.['isoMetadata.termsOfUseId'];
-      const privacy = extraParams?.['isoMetadata.privacy'];
-      const isRequired = privacy === 'NONE' && !!termsOfUseId && termsOfUseId !== 1;
+      const isRequired = termsOfUseId !== 1;
       if (isRequired && !isDefined(val)) {
         return {
           valid: false,
